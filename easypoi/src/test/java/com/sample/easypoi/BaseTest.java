34a9acaf4f8c3428fb63fec6739d6f9ca0e98ada
Dream <openslc@163.com>
2018-03-13 16:14:18 +0800

    1、修改RESOURCE_PATH为相对路径

--- easypoi/src/test/java/com/sample/easypoi/BaseTest.java
+++ easypoi/src/test/java/com/sample/easypoi/BaseTest.java
@@ -1,5 +1,14 @@
 package com.sample.easypoi;
 
+import org.junit.Test;
+
 public class BaseTest {
-    public static final String RESOURCE_PATH = "/Users/sunguangzhu/develop/github/sample/easypoi/src/main/resources";
+    public static final String RESOURCE_PATH =  System.getProperty("user.dir")+"/src/main/resources";
+
+    @Test
+    public void test01() throws Exception{
+        System.out.println("RESOURCE_PATH = " + RESOURCE_PATH);
+        String property = System.getProperty("user.dir");
+        System.out.println("property = " + property);
+    }
 }
