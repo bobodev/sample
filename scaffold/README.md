7ed6f855b4a0542f18131d05e30cb8c28a060128
Dream <openslc@163.com>
2018-03-16 13:54:43 +0800

    初始化脚手架项目

---
+++ scaffold/README.md
@@ -0,0 +1,8 @@
+## scaffold
+
+本文主要描述脚手架项目的相关内容
+
+项目git地址 https://github.com/gatdevelop/sample.git
+
+## 1. 概述
+
--- scaffold/src/test/java/com/sample/scaffold/service/biz/EchoServiceTest.java
+++ scaffold/src/test/java/com/sample/scaffold/service/biz/EchoServiceTest.java
@@ -3,14 +3,19 @@ package com.sample.scaffold.service.biz;
 import com.sample.scaffold.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 public class EchoServiceTest {
+    @Autowired
+    private IEchoService echoService;
+
     @Test
     public void echo() throws Exception {
+        echoService.echo();
     }
 
 }
