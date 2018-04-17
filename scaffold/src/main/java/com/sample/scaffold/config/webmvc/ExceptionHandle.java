74f90d415fb58dff828532c3dc4096deb19d7e77
Ryan.Sun <openslc@163.com>
2018-04-17 15:17:20 +0800

    修改Readme.md 支持方法级别参数校验

--- scaffold/src/main/java/com/sample/scaffold/config/webmvc/ExceptionHandle.java
+++ scaffold/src/main/java/com/sample/scaffold/config/webmvc/ExceptionHandle.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.lang.reflect.UndeclaredThrowableException;
+
 @Configuration
 @ControllerAdvice
 public class ExceptionHandle {
@@ -14,10 +16,12 @@ public class ExceptionHandle {
     @ExceptionHandler(value = {Exception.class})
     @ResponseBody
     public ResponseEntity<Object> jsonErrorHandler(Exception exception) throws Exception {
-        exception.printStackTrace();
         ServiceException serviceException;
         if (exception instanceof ServiceException) {
             serviceException = (ServiceException) exception;
+        } else if (exception instanceof UndeclaredThrowableException) {
+            Throwable cause = exception.getCause();
+            serviceException = new ServiceException(cause.getMessage());
         } else {
             serviceException = new ServiceException(exception);
             serviceException.setMessage(exception.getMessage());
