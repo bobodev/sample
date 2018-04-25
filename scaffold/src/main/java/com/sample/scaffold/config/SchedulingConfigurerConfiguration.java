9693e9050ce1203f55ededffe86c386c458944d8
Ryan.Sun <openslc@163.com>
2018-04-25 14:41:24 +0800

    定时任务的相关例子

--- scaffold/src/main/java/com/sample/scaffold/config/SchedulingConfigurerConfiguration.java
+++ scaffold/src/main/java/com/sample/scaffold/config/SchedulingConfigurerConfiguration.java
@@ -1,13 +1,11 @@
 package com.sample.scaffold.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
-@EnableScheduling
 public class SchedulingConfigurerConfiguration implements SchedulingConfigurer {
 
     @Override
